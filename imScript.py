cdd0da9f757eec3f8e0a7035b01f7118bbf81061
Daria <daria@Darias-MacBook-Pro.local>
2017-11-10 14:15:34 +0000

    removed image save, added region loop

--- imScript.py
+++ imScript.py
@@ -3,12 +3,19 @@ import nibabel as nib
 import numpy as np
 from PIL import Image
 from itertools import product
+import math
 
 filenames = os.listdir("Data")
 # print filenames
 
 T1w_restore_brain_file = os.path.join("Data", 'sub-CC00060XX03_ses-12501_T1w_restore_brain.nii.gz')
 T1w_im = nib.load(T1w_restore_brain_file)
+
+units = T1w_im.header.get_xyzt_units() #mm, sec
+dimensions = T1w_im.header.get_zooms() #0.5mm each way
+
+#convert dimensions to m
+dimensions = np.divide(dimensions, 100)
 
 Tissue_labels_file = os.path.join("Data", 'sub-CC00060XX03_ses-12501_drawem_tissue_labels.nii.gz')
 tissue_labels = nib.load(Tissue_labels_file)
@@ -23,12 +30,18 @@ affine = tissue_labels.affine
 t1_data = T1w_im.get_data()
 
 # Single array of ROI data for 1 dataset
-t1_all = np.zeros((9,shape[0],shape[1],shape[2]), dtype=np.float32)
-
+"""t1_all = np.zeros((9,shape[0],shape[1],shape[2]), dtype=np.float32)
 
 for i, j, k in product(xrange(shape[0]), xrange(shape[1]), xrange(shape[2])):
 
     # Find mask value aka. region type 1-9
     val = tissue_data[i][j][k]
     # save t1 data to region type, coords
-    t1_all[int(val)-1][i][j][k] = t1_data[i][j][k]
+    t1_all[int(val)-1][i][j][k] = t1_data[i][j][k]"""
+
+# loop over regions
+for region in (range(min(tissue_data),max(tissue_data)+1)) :
+    t1_region = t1_data(tissue_data == region)
+    t1_region = t1_region[t1_region>0] # remove any 0 values
+    t1_avg_intensity = math.mean(t1_region)
+    t1_vol = reduce(lambda x, y: x*y, t1_region)*reduce(lambda x, y: x*y, dimensions)
