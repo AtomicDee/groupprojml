ba44347ce9620aa4121e0e5a2418ffa4c1ee6875
Avinasho <Avinasho>
2018-03-02 11:55:40 +0000

    Added the script to separate the features from the csv

--- RF_Regression.py
+++ RF_Regression.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import os
-# from sklearn import decomposition
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import tree
 # import nibabel
@@ -9,39 +8,22 @@ from sklearn import tree
 # import sys
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-path = '/home/avi/Desktop/Reformatted_data.csv'
 import matplotlib.pyplot as plt
+
+# path = '/home/avi/Desktop/Reformatted_data.csv'
+# path = '/home/avi/Desktop/all_normalised_data.csv'
+path = '/home/avi/Desktop/GROUP_PROJ_DATA'
 # The path where all the data is held
 
-data = pd.read_csv(path)
-# Using pandas to read all the data in
-
-T1 = []
-T2 = []
-Volume = []
-PatCode = []
-SessID = []
-BirthAge = []
-ScanAge = []
-Gender = []
-
-for row in data.itertuples():
-    PatCode.append(pd.DataFrame([row[2]]))
-    SessID.append(pd.DataFrame([row[3]]))
-    BirthAge.append(pd.DataFrame([row[4]]))
-    ScanAge.append(pd.DataFrame([row[5]]))
-    Gender.append(pd.DataFrame([row[6]]))
-    T1.append(pd.DataFrame([row[7:94]]))
-    T2.append(pd.DataFrame([row[95:182]]))
-    Volume.append(pd.DataFrame([row[183:270]]))
-PatCode = np.ravel( pd.concat( PatCode ) )
-SessID = np.ravel( pd.concat( SessID ) )
-BirthAge = np.ravel( pd.concat( BirthAge ) )
-ScanAge = np.ravel( pd.concat( ScanAge ) )
-Gender = np.ravel( pd.concat( Gender ) )
-T1 = pd.concat(T1)
-T2 = pd.concat(T2)
-Volume = pd.concat(Volume)
+T1 = pd.read_csv(path + '/T1.csv')
+T2 = pd.read_csv(path + '/T2.csv')
+Volume = pd.read_csv(path + '/Volume.csv')
+PatCode = pd.read_csv(path + '/PatCode.csv')
+SessID = pd.read_csv(path + '/SessID.csv')
+BirthAge = pd.read_csv(path + '/BirthGA.csv')
+ScanAge = pd.read_csv(path + '/ScanGA.csv')
+Gender = pd.read_csv(path + '/Gender.csv')
+all_feat = pd.read_csv(path + '/T1_T2_Vol.csv')
 
 training_data = []
 testing_data = []
@@ -61,63 +43,80 @@ table = np.zeros((np.size(depth), np.size(estim)))
 best_depth = 0
 best_estim = 0
 curr_max = -10000
+#
+# for i in range(len(depth)):
+#     curr_score = []
+#     for j in range(len(estim)):
+#         clf = RandomForestRegressor(max_depth = depth[i], max_features = 16,
+#                                     n_estimators = estim[j])
+#         clf.fit(training_data, training_labels)
+#         prediction = clf.predict(testing_data)
+#         curr_score = clf.score(prediction, testing_labels)
+#         table[i][j] = curr_score
+#         if curr_score > curr_max:
+#             curr_max = curr_score
+#             best_depth = depth[i]
+#             best_estim = estim[i]
+# print ' '
+# print 'The best accuracy score found was: ', curr_max
+# print 'The optimal depth found was: ', best_depth
+# print 'The optimal number of estimators was found to be: ', best_estim
+# print ' '
+# # print table
+#
+# # no. features = 261 --> max_features = sqrt(261)~=16
+# clf_opt = RandomForestRegressor(max_depth = best_depth, max_features = 16,
+#                                 n_estimators = best_estim)
+# clf_opt.fit(training_data, training_labels)
+#
+# # Predict
+# train_pred = clf_opt.predict(testing_data)
+# pred = clf_opt.predict(testing_data)
+#
+# print np.shape(pred)
+# print np.shape(testing_labels)
+# print 'ACCURACY SCORE :', accuracy_score(pred, testing_labels)
+# print ' '
+#
+# # print np.shape(training_data)
+# # print np.shape(testing_data)
+# # print np.shape(training_labels)
+# # print np.shape(testing_labels)
+# # print np.shape(pred)
+# # print np.shape(train_pred)
+#
+# plt.figure(1)
+# plt.scatter(pred, testing_labels, c="k", label='Prediction')
+# # plt.scatter(pred, training_labels, c='g', label ='Training Samples')
+# plt.xlabel("data")
+# plt.ylabel("target")
+# plt.title("Random Forest Regression")
+# plt.legend()
+# # plt.show()
+#
+# plt.figure(2)
+# # plt.scatter(pred, testing_labels, c="k", label='Prediction')
+# plt.scatter(train_pred, training_labels, c='g', label ='Training Samples')
+# plt.xlabel("data")
+# plt.ylabel("target")
+# plt.title("Random Forest Regression")
+# plt.legend()
+# plt.show()
 
-for i in range(len(depth)):
-    curr_score = []
-    for j in range(len(estim)):
-        clf = RandomForestRegressor(max_depth = depth[i], max_features = 2,
-                                    n_estimators = estim[j])
-        clf.fit(training_data, training_labels)
-        curr_score = clf.score(testing_data, testing_labels)
-        table[i][j] = curr_score
-        if curr_score > curr_max:
-            curr_max = curr_score
-            best_depth = depth[i]
-            best_estim = estim[i]
-# print table
-print ' '
-print 'The best accuracy score found was: ', curr_max
-print 'The optimal depth found was: ', best_depth
-print 'The optimal number of estimators was found to be: ', best_estim
-print ' '
-# print table
 
-clf_opt = RandomForestRegressor(max_depth = best_depth, max_features = 2,
-                                n_estimators = best_estim)
-clf_opt.fit(training_data, training_labels)
 
-# Predict
-pred = clf_opt.predict(testing_data)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #
-# clf_opt.fit(pred.reshape(1, -1), training_labels)
-# pred.score = clf_opt.score(pred.reshape(-1,1), testing_labels)
-# # print 'The prediction based on the training data is: ', pred
-#
-print np.shape(training_data)
-print np.shape(testing_data)
-print np.shape(training_labels)
-print np.shape(testing_labels)
-print np.shape(pred)
-print np.shape(T1)
-#
-plt.figure()
-plt.scatter(pred, testing_labels, c="k", label='Prediction')
-plt.scatter(pred, training_labels, c='g', label ='Training Samples')
-# plt.plot(testing_data, pred, c="g", label="n_estimators=1", linewidth=2)
-plt.xlabel("data")
-plt.ylabel("target")
-plt.title("Random Forest Regression")
-plt.legend()
-plt.show()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# #
---
+++ separating_data.py
@@ -0,0 +1,49 @@
+#
+# data = pd.read_csv(path)
+# Using pandas to read all the data in
+
+
+# T1 = []
+# T2 = []
+# Volume = []
+# PatCode = []
+# SessID = []
+# BirthAge = []
+# ScanAge = []
+# Gender = []
+# all_feat = []
+#
+# for row in data.itertuples():
+#     PatCode.append(pd.DataFrame([row[1]]))
+#     SessID.append(pd.DataFrame([row[2]]))
+#     BirthAge.append(pd.DataFrame([row[3]]))
+#     ScanAge.append(pd.DataFrame([row[4]]))
+#     Gender.append(pd.DataFrame([row[5]]))
+#     T1.append(pd.DataFrame([row[6:93]]))
+#     T2.append(pd.DataFrame([row[93:180]]))
+#     Volume.append(pd.DataFrame([row[180:270]]))
+#     all_feat.append(pd.DataFrame([row[6:270]]))
+# # PatCode = np.ravel( pd.concat( PatCode ) )
+# # SessID = np.ravel( pd.concat( SessID ) )
+# # BirthAge = np.ravel( pd.concat( BirthAge ) )
+# # ScanAge = np.ravel( pd.concat( ScanAge ) )
+# # Gender = np.ravel( pd.concat( Gender ) )
+# PatCode = pd.concat( PatCode )
+# SessID = pd.concat( SessID )
+# BirthAge = pd.concat( BirthAge )
+# ScanAge = pd.concat( ScanAge )
+# Gender = pd.concat( Gender )
+# T1 = pd.concat(T1)
+# T2 = pd.concat(T2)
+# Volume = pd.concat(Volume)
+# all_feat = pd.concat(all_feat)
+#
+# PatCode.to_csv('PatCode.csv')
+# SessID.to_csv('SessID.csv')
+# BirthAge.to_csv('BirthGA.csv')
+# ScanAge.to_csv('ScanGA.csv')
+# Gender.to_csv('Gender.csv')
+# T1.to_csv('T1.csv')
+# T2.to_csv('T2.csv')
+# Volume.to_csv('Volume.csv')
+# all_feat.to_csv('T1_T2_Vol.csv')
