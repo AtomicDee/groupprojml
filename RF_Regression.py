338f22c077cfdb46f9aed3bb1a6e42fb70d65cfb
Avinasho <Avinasho>
2018-03-08 12:48:39 +0000

    added feature extraction

--- RF_Regression.py
+++ RF_Regression.py
@@ -30,14 +30,20 @@ training_data, testing_data, training_labels, testing_labels = train_test_split(
 # to the training data, so that when being run the clf becomes crappy at fitting
 # test data
 
-depth = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21] # 11(.score()) # 15(cross_val_score())
-estim = [10, 30, 50, 100, 300, 500, 800, 1000] # 100(.score()) # 300(cross_val_score())
+# depth = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21] # 11(.score()) # 15(cross_val_score())
+# estim = [10, 30, 50, 100, 300, 500, 800, 1000] # 100(.score()) # 300(cross_val_score())
 # table = np.zeros((np.size(depth), np.size(estim)))
 #
 # best_depth = 0
 # best_estim = 0
-best_depth = 15
-best_estim = 300
+#
+# best_depth = 21 #0.9
+# best_estim = 30 #0.9
+best_depth = 15 #0.5
+best_estim = 300 #0.5
+# best_depth = 15 #0.8
+# best_estim = 100 #0.8
+#
 # curr_max = -10000
 # curr_max_std = -10000
 #
@@ -64,8 +70,11 @@ best_estim = 300
 # print 'The optimal number of estimators was found to be: ', best_estim
 # print ' '
 # # print table
-
+#
+# raw_input()
+#
 # no. features = 261 --> max_features = sqrt(261)~=16
+
 regr_opt = RandomForestRegressor(max_depth = best_depth, max_features = 16,
                                 n_estimators = best_estim)
 regr_opt.fit(training_data, training_labels)
@@ -77,33 +86,39 @@ pred = regr_opt.predict(testing_data)
 curr_score = cross_val_score(regr_opt, testing_data, testing_labels)
 curr_mean = np.mean(curr_score)
 curr_std = np.std(curr_score)
+feat_imp = regr_opt.feature_importances_
+
+T1_feature_scores = feat_imp[1:88]
+T2_feature_scores = feat_imp[88:175]
+Vol_feature_scores = feat_imp[175:]
 
 print ' '
 print 'Cross-Valdation Score: ', curr_mean
 print 'With a standard deviation of: ', curr_std
+print type(feat_imp)
+print type(T1_feature_scores)
 print ' '
 
-# print ' '
-# print 'size of training data: ', np.shape(training_data)
-# print 'size of testing data: ', np.shape(testing_data)
-# print 'size of training labels: ', np.shape(training_labels)
-# print 'size of testing labels: ', np.shape(testing_labels)
-# print 'size of testing prediction: ', np.shape(pred)
-# print 'size of training prediction: ', np.shape(train_pred)
-# print ' '
+np.savetxt('T1_features.csv', T1_feature_scores, delimiter=",")
+np.savetxt('T2_features.csv', T2_feature_scores, delimiter=",")
+np.savetxt('Vol_features.csv', Vol_feature_scores, delimiter=",")
+# T1_feature_scores.to_csv('T1_features.csv')
+# T2_feature_scores.to_csv('T2_features.csv')
+# Vol_feature_scores.to_csv('Vol_features.csv')
 
-plt.figure(1)
-plt.scatter(pred, testing_labels, c="k", label='Prediction')
-plt.xlabel("data")
-plt.ylabel("target")
-plt.title("Random Forest Regression")
-plt.legend()
-plt.show()
 
-plt.figure(2)
-plt.scatter(train_pred, training_labels, c='g', label ='Training Samples')
-plt.xlabel("data")
-plt.ylabel("target")
-plt.title("Random Forest Regression")
-plt.legend()
-plt.show()
+# plt.figure(1)
+# plt.scatter(pred, testing_labels, c="k", label='Prediction')
+# plt.xlabel("data")
+# plt.ylabel("target")
+# plt.title("Random Forest Regression")
+# plt.legend()
+# plt.show()
+#
+# plt.figure(2)
+# plt.scatter(train_pred, training_labels, c='g', label ='Training Samples')
+# plt.xlabel("data")
+# plt.ylabel("target")
+# plt.title("Random Forest Regression")
+# plt.legend()
+# plt.show()
