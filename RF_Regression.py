03e8573aa85a992176222c06f3b59ab1c68dc04b
Avinasho <Avinasho>
2018-03-09 13:43:47 +0000

    small updates

--- RF_Regression.py
+++ RF_Regression.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 
 # path = '/home/avi/Desktop/all_normalised_data.csv'
-path = '/home/avi/Desktop/GROUP_PROJ_DATA'
+path = '/home/avi/Desktop/groupprojml/GROUP_PROJ_DATA'
 
 T1 = pd.read_csv(path + '/T1.csv')
 T2 = pd.read_csv(path + '/T2.csv')
@@ -25,19 +25,19 @@ training_data = []
 testing_labels = []
 training_labels = []
 
-# training_data, testing_data, training_labels, testing_labels = train_test_split(all_feat, ScanAge, train_size=0.8)
 # the problem with setting some parameters too high is that it becomes overfit
 # to the training data, so that when being run the clf becomes crappy at fitting
 # test data
 
-# depth = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21] # 11(.score()) # 15(cross_val_score())
-# estim = [10, 30, 50, 100, 300, 500, 800, 1000] # 100(.score()) # 300(cross_val_score())
+depth = [5, 7, 9, 11, 15, 17, 19, 21] # 11(.score()) # 15(cross_val_score())
+estim = [30, 50, 100, 300, 500, 800] # 100(.score()) # 300(cross_val_score())
+training_size = [0.5, 0.7, 0.8, 0.9]
+
 # depth = [11, 15, 17, 19, 21] # 11(.score()) # 15(cross_val_score())
 # estim = [30, 50, 100, 300, 500, 800] # 100(.score()) # 300(cross_val_score())
 # depth = [15]
 # estim = [50]
-# training_size = [0.5, 0.7, 0.8, 0.9]
-# table = np.zeros((np.size(depth), np.size(estim)))
+table = np.zeros((np.size(depth), np.size(estim)))
 
 # best_depth = 21 #0.9
 # best_estim = 30 #0.9
@@ -47,57 +47,98 @@ training_labels = []
 # best_depth = 15 #0.8
 # best_estim = 100 #0.8
 #
-# curr_max = -10000
-# curr_max_std = -10000
+curr_max = -10000
+curr_max_std = -10000
 # ave = [None] * 5
+
+all_best_sizes = []
+all_best_depths = []
+all_best_estims = []
+all_best_scores = []
+all_best_cross_scores = []
 
 ############################################################ Training size:  0.9
 ##################################################################### Depth:  15
 ################################################################ Estimation:  50
-best_depth = 100
-best_estim = 50
-training_size = 90
-training_data, testing_data, training_labels, testing_labels = train_test_split(all_feat, ScanAge, train_size=training_size)
+# best_depth = 100
+# best_estim = 50
+# training_size = 0.9
+# training_data, testing_data, training_labels, testing_labels = train_test_split(all_feat, ScanAge, train_size=training_size)
 
-# for x in range(0, 100):
-# for k in range(len(training_size)):
-#     training_data, testing_data, training_labels, testing_labels = train_test_split(all_feat, ScanAge, train_size=training_size[k])
-#     for i in range(len(depth)):
-#         curr_score = []
-#         for j in range(len(estim)):
-#             # print ' '
-#             # print 'Training size: ', training_size[k]
-#             # print 'Depth: ', depth[i]
-#             # print 'Estimation: ', estim[j]
-#             regr = RandomForestRegressor(max_depth = depth[i], max_features = 16,
-#                                         n_estimators = estim[j])
-#             regr.fit(training_data, training_labels)
-#             # curr_score = clf.score(testing_data, testing_labels)
-#             curr_score = cross_val_score(regr, testing_data, testing_labels)
-#             curr_mean = np.mean(curr_score)
-#             curr_std = np.std(curr_score)
-#             # raw_input()
-#             table[i][j] = curr_mean
-#             if curr_mean > curr_max:
-#                 curr_max = curr_mean
-#                 curr_max_std = curr_std
-#                 best_depth = depth[i]
-#                 best_estim = estim[j]
-#                 best_size = training_size[k]
-#                 print ' '
-#                 print 'Training size: ', training_size[k]
-#                 print 'Depth: ', depth[i]
-#                 print 'Estimation: ', estim[j]
+# testing_data = testing_data[testing_data.columns[1:]]
+# testing_labels = testing_labels[testing_labels.columns[1:]]
 #
-#     # ave[x] = best_size
-#
-# print ' '
-# print 'The best accuracy score found was: ', curr_max
-# print 'The optimal depth found was: ', best_depth
-# print 'The optimal number of estimators was found to be: ', best_estim
-# # print 'The optimal training size is: ', np.mean(ave)
-# print ' '
-# print table
+# training_data = training_data[training_data.columns[1:]]
+# training_labels = training_labels[training_labels.columns[1:]]
+
+for k in range(len(training_size)):
+    training_data, testing_data, training_labels, testing_labels = train_test_split(all_feat, ScanAge, train_size=training_size[k])
+    for i in range(len(depth)):
+        curr_score = []
+        for j in range(len(estim)):
+            regr = RandomForestRegressor(max_depth = depth[i], max_features = 16,
+                                        n_estimators = estim[j])
+            regr.fit(training_data, training_labels)
+            curr_score = regr.score(testing_data, testing_labels)
+            curr_cross_score = cross_val_score(regr, testing_data, testing_labels)
+            curr_mean = np.mean(curr_cross_score)
+            curr_std = np.std(curr_cross_score)
+            table[i][j] = curr_mean
+            if curr_mean > curr_max:
+                curr_cross_max = curr_mean
+                curr_max = curr_score
+                curr_max_std = curr_std
+                best_depth = depth[i]
+                best_estim = estim[j]
+                best_size = training_size[k]
+                print ' '
+                print 'Training size: ', training_size[k]
+                print 'Depth: ', depth[i]
+                print 'Estimation: ', estim[j]
+                print 'Cross val score: ', curr_cross_max
+                print 'Cross val std: ', curr_max_std
+                print 'Score: ', curr_max
+
+print ' '
+print 'The best cross score found was: ', curr_cross_max
+print 'With a STD of: ', curr_max_std
+print 'The best score is: ', curr_max
+print 'The optimal depth found was: ', best_depth
+print 'The optimal number of estimators was found to be: ', best_estim
+print 'The best training size is: ', best_size
+# print 'The optimal training size is: ', np.mean(ave)
+print table
+print ' '
+
+# The best cross score found was:  0.900382049012
+# With a STD of:  0.0211345141403
+# The best score is:  0.905793153858
+# The optimal depth found was:  11
+# The optimal number of estimators was found to be:  300
+# The best training size is:  0.5
+# best cross score found was:  0.911645462341
+# With a STD of:  0.0249744785107
+# The best score is:  0.925722181604
+# The optimal depth found was:  5
+# The optimal number of estimators was found to be:  30
+# The best training size is:  0.7
+# The best cross score found was:  0.871062026774
+# With a STD of:  0.0140868768709
+# The best score is:  0.910451892723
+# The optimal depth found was:  5
+# The optimal number of estimators was found to be:  30
+# The best training size is:  0.8
+# The best cross score found was:  0.882416262025
+# With a STD of:  0.0172555872232
+# The best score is:  0.901312736719
+# The optimal depth found was:  5
+# The optimal number of estimators was found to be:  30
+# The best training size is:  0.7
+
+
+
+
+
 
 # no. features = 261 --> max_features = sqrt(261)~=16
 
@@ -135,14 +176,19 @@ print 'With a standard deviation of: ', curr_std
 # print ' '
 # print pred
 # print ' '
+# print testing_labels[testing_labels.columns[1:]]
+# print type(training_data)
+# new_testing_data = testing_data[testing_data.columns[1:]]
+# print training_labels
+#
 # print testing_labels
-
 plt.figure(1)
-plt.scatter(pred, testing_labels, c="k", label='Prediction')
-plt.xlabel("data")
-plt.ylabel("target")
+plt.scatter(pred, testing_labels, c='b', label='Prediction')
+plt.plot([0, 50], [0, 50], '--k')
+plt.xlabel('Data')
+plt.ylabel('Target')
 plt.xlim([28, 45])
 plt.ylim([25, 50])
-plt.title("Random Forest Regression")
+plt.title('Random Forest Regression')
 plt.legend()
 plt.show()
